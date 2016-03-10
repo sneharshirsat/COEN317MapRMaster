311b03bd5fc82193230a009ec78d85edd2a6d7ea
nishantvphatangare@gmail.com <nishant1914@gmail.com>
2016-03-10 02:07:35 -0800

    ClientMapper Written

--- COEN317MapRMaster/src/module/communication/ClientMapperThread.java
+++ COEN317MapRMaster/src/module/communication/ClientMapperThread.java
@@ -5,6 +5,16 @@
  */
 package module.communication;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author nishant
  *
@@ -13,6 +23,13 @@ public class ClientMapperThread implements Runnable {
 	
 	private Thread t;
 	private String threadName = "Client Mapper Thread";
+	
+	private ServerSocket serverSocket;
+	private Socket clientSocket;
+	private int StoCPort=50000;
+	
+	private String chunkToRead = "/home/nishant/Desktop/COEN317/chunksebook/1.txt";
+	
 
 	/* (non-Javadoc)
 	 * @see java.lang.Runnable#run()
@@ -30,6 +47,76 @@ public class ClientMapperThread implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		System.out.println("--Running " +  threadName );
+		
+		
+		try {
+	        serverSocket = new ServerSocket(StoCPort);  //Server socket
+	        
+
+	    } catch (IOException e) {
+	        System.out.println("Could not listen on port: "+StoCPort);
+	    }
+
+	    System.out.println("Server started. Listening to the port "+StoCPort);
+
+	    while (true) {
+	        
+	    	try {
+	        	
+	            clientSocket = serverSocket.accept();   //accept the client connection
+	            
+                // sending to client (pwrite object)
+	            OutputStream ostream = clientSocket.getOutputStream(); 
+	            PrintWriter pwrite = new PrintWriter(ostream, true);
+				
+				// receiving from server ( receiveRead  object)
+				InputStream istream = clientSocket.getInputStream();
+				BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+				
+				String receiveMessage;
+				String sendMessage = "This is from PC by Nishant";
+				  
+				// The name of the file to open.
+			     //String fileName = this.fileName;//"/home/nishant/Documents/OSfilesendTest.txt";
+			     String line2 = null;
+				  
+				  // FileReader reads text files in the default encoding.
+		            FileReader fileReader = 
+		                new FileReader(chunkToRead);
+
+		            // Always wrap FileReader in BufferedReader.
+		            BufferedReader bufferedReader = 
+		                new BufferedReader(fileReader);
+		            
+		            String line3 = "---fileSendingFinishedByServer---";
+		            System.out.println("file being sent - " +chunkToRead); 
+		            while((line2 = bufferedReader.readLine()) != null) {
+					  //line3 = line3 + line2;
+					  pwrite.println(line2);             
+					  pwrite.flush();
+					  //System.out.println(line2);
+		            } 	
+		            // Always close files.
+		            bufferedReader.close();  
+		            pwrite.println(line3);             
+					pwrite.flush();
+				  
+				  while(true)
+				  {
+				    if((receiveMessage = receiveRead.readLine()) != null)  
+				    {
+				       System.out.println("Current Count: " +receiveMessage);  
+				       //long countInThisFile = processReceivedResult(receiveMessage);
+				       //System.out.println("Total Count so far: " +countInThisFile);
+				    }         
+				   
+				  }               
+			
+	        } catch (IOException ex) {
+	            System.out.println("Problem in message reading");
+	        }
+	        
+	    }
 
 	}
 
